afe2894c4639d586d658bfedc555f5e72c31de9d
Recep Aydoğan <raydogan20@posta.pau.edu.tr>
2024-03-26 17:55:38 +0300

    useTransition simple usage

--- useTransition/src/App.jsx
+++ useTransition/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useTransition } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [Numbers, setNumbers] = useState([]);
+  const [list, setList] = useState([]);
+  const [isPending, startTransition] = useTransition();
+  const handleChange = (e) => {
+    const myList = [];
+    setNumbers(e.target.value);
+    startTransition(() => {
+      for (let i = 0; i < 2000; i++) {
+        myList.push(e.target.value);
+      }
+      setList(myList);
+    });
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" value={Numbers} onChange={handleChange} />
+      {isPending
+        ? "Loading..."
+        : list.map((number, index) => <div key={index}>{number}</div>)}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
